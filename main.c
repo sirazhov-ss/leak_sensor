e11a5311b75c1217b14ced642d724d9349530596
Said <said_box@mail.ru>
2023-01-09 17:31:51 +0300

    Add recieve data

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #define BAUDRATE (F_CPU/(16*4800L))-1
 #define INT_LEAK (PIND&(1<<PD2))
 #define INT_LB (PIND&(1<<PD3))
+#define WAIT 1000000UL
 
 #include <avr/io.h>
 #include <string.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+
+int stop_key = 0;
 
 void PIN_Init(void){
 	DDRA |= ((1<<0) | (1<<1));
@@ -16,16 +19,34 @@ void PIN_Init(void){
 	PORTD |= ((1<<2) | (1<<3));
 }
 
+ISR(USART_RX_vect){
+	stop_key = UDR;
+}
+
 void UART_Init(unsigned int baud){
 	UBRRH = (unsigned char)(baud>>8);
 	UBRRL = (unsigned char)baud;
 	UCSRB = (1<<RXEN)|(1<<TXEN);
 	UCSRC = (1<<USBS)|(3<<UCSZ0);
+	UCSRB |= (1<<RXCIE);
+	sei();
 }
 
 void UART_Transmit(unsigned char data){
 	while (!(UCSRA & (1<<UDRE)));
 	UDR = data;
+}
+
+unsigned char UART_Receive(void){
+	while (!(UCSRA & (1<<RXC)));
+	return UDR;
+}
+
+void USART_Flush( void )
+{
+	while ( UCSRA & (1<<RXC) ) {
+		unsigned char dummy = UDR;
+	}
 }
 
 void send_message(char* message){
@@ -43,16 +64,24 @@ int main(void)
 	
 	char* response[2] = {"Leak is detected!", "No leak!"};
 	int i = 0;
+	int counter = WAIT;
 	
     while (1) 
     {
 		PORTA ^= ((1<<0) | (1<<1));
-		send_message(response[i]);
-		_delay_ms(500);
-		i++;
-		if (i > 1) {
-			i = 0;
+		if (stop_key == '1' || counter <= 0) {
+			send_message(response[i]);
+			i++;
+			if (i > 1) {
+				i = 0;
+			}
+			if (counter <= 0) {
+				send_message("counter is less than zero..");
+				counter = WAIT;
+			}
+			
 		}
+		counter--;
     }
 }
 
